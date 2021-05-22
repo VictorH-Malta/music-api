b4d0644da0aa294c929065f30172889fc4358e8a
VictorH-Malta <victor.fobos@gmail.com>
2021-05-22 15:43:52 -0300

    create vagalume proxy api

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const express = require('express')
+const axios = require('axios');
+const app = express()
+
+app.use(express.json())
+
+app.get('/artists/:name', async function (req, res) {
+	const response = await axios.get(`https://vagalume.com.br/${req.params.name}/index.js`);
+	res.json(response.data);
+})
+
+app.listen(3000, function () {
+	console.log('Rodando o servidor em http://localhost:3000');
+})
+
